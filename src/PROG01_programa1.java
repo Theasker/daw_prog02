654356fcc3ddbccf086de7c23b24fe61f983fce6
Theasker <>
2012-11-12 11:58:56 +0100

    Tarea02 - Terminado ejercicio variables

---
+++ src/PROG01_programa1.java
@@ -0,0 +1,13 @@
+/**
+* Primer porgrama de curso DAW online
+*/
+class PROG01_programa1 {
+	public static void main(String[] args) {
+		java.lang.System.out.println("UNIDAD DE TRABAJO 01");
+		java.lang.System.out.println("Introducción a la programación");
+		java.lang.System.out.println("Mauricio Segura Ariño");
+		java.lang.System.out.println("Zaragoza (Zaragoza)");
+		java.lang.System.out.println("30 de septiembre de 2012");
+		java.lang.System.out.println("Programa1");
+	}
+}
---
+++ src/temp.java
@@ -0,0 +1,18 @@
+/*
+operadoresaritmeticos.java
+Programa que muestra el uso de los operadores aritméticos
+/*
+public class operadoresaritmeticos {
+public static main(String[] args) 
+short x = 7;
+int y = 5;
+float f1 = 13.5;
+float f2 = 8f;
+System.out.println("El valor de x es ", x, " y el valor de y es ", y);
+System.out.println("El resultado de x + y es " + (x + y));
+System.out.println("El resultado de x - y es " + (x - y));
+System.out.printf("\n%s%s\n","División entera:","x / y = ",(x/y));
+System.out.println("Resto de la división entera: x % y = " + (x % y));
+System.out.printf("El valor de f1 es %f y el de f2 es %f\n",f1,f2);
+System.out.println("El resultado de f1 / f2 es " + (f1 / f2))
+}
